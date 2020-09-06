43a9c8541c8b55e17b2e26014549bd5806ea9371
Mohit Tanwar <70837575+mohittanwarcomviva@users.noreply.github.com>
2020-09-06 20:04:44 +0530

    Update data.java

--- data.java
+++ data.java
@@ -3,6 +3,6 @@ import java.util.*;
 class Data{
        
        public static void main(String[] args){
-             System.out.println("data");
+             System.out.println("Raju");
        }
 }
